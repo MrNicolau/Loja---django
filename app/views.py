7e1719e52de4ba6fea502703cca0292f1708aebb
Igor Nicolau <igornicolaueng@gmail.com>
2025-01-18 11:57:00 -0300

    18-01-2025

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView,UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Product
 
 
@@ -8,3 +9,32 @@ class ProductListView(ListView):
     context_object_name = 'products'
     paginate_by = 10
 
+#CRUD
+
+#CREATE
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['name','image','brand','category','description','price','countInStock','user']
+    template_name = "product_create_update.html"
+    success_url = reverse_lazy('product-list')
+
+
+#READ
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "produtos_detail.html"
+    context_object_name = 'product'
+
+#UPDATE
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['name','image','brand','category','description','price','countInStock','user']
+    template_name = "product_create_uptade.html"
+    success_url = reverse_lazy('product-list')
+
+
+#DELETE
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = "product_confirm_delete.html"
+    success_url = reverse_lazy('product-list')
